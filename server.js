a73dcee5ee27ce0ac7be3539faa4f529df4fdf85
hautrvo <69573672+hautrvo@users.noreply.github.com>
2020-08-12 18:13:02 +0700

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+
+const mockUserData = [
+    {
+        name: 'Mark'
+    },{
+        name: 'Jill'
+    }
+]
+
+app.get('/users', function(){
+    resizeBy.json({
+        success: true,
+        message: 'successfully got users. Nice!',
+        users: mockUserData
+    })
+});
+
 app.listen(8000, function(){
-    console.log("server is running");
+    console.log("server is listening");
 })
